cc2e222225fe6df2244c579634378d9f2a6d235c
fasilkp <fasilkp314@gmail.com>
2023-03-11 11:34:29 +0530

    userRegister controller created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -35,6 +35,35 @@ export async function userRegister(req, res) {
     }
 }
 
+export async function userLogin(req, res) {
+    try {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email })
+        if (!user)
+            return res.json({ error: true, message: "No User found" })
+        const userValid = bcrypt.compareSync(password, user.password);
+        if (!userValid)
+            return res.json({ error: true, message: "wrong Password" })
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            "myjwtsecretkey"
+        )
+        console.log(token)
+        // const exp= new Date()+ 1000*60;
+        return res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            maxAge: 1000 * 60 * 60 * 24 * 7 * 30,
+            sameSite: "none",
+        }).json({ error: false, user: user._id })
+    }
+    catch (err) {
+        res.json({ message: "server error", error: err })
+        console.log(err);
+    }
+}
 
 
 
