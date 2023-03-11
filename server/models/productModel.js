aec331c551ca23c86c4c6cfc56d6cbf5c4dd0e9c
fasilkp <fasilkp314@gmail.com>
2023-03-11 13:51:24 +0530

    porduct model created

---
+++ server/models/productModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    userId:{
+        type:String,
+        required:true
+    }
+})
+
+const productModel = mongoose.model("product", productSchema)
+export default productModel
